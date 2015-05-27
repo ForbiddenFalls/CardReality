8853db5fabd734d23b17e85ba5b2be0493ba5d49
JulietaAtanasova <julieta.p.atanasova@gmail.com>
2015-05-27 23:56:31 +0300

    Add players ranking

---
+++ CardReality/Controllers/RankingController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CardReality.Data.Data;
+
+namespace CardReality.Controllers
+{
+    public class RankingController : BaseController
+    {
+
+        public RankingController(IApplicationData data) : base(data)
+        {
+        }
+
+        // GET: Ranking
+        public ActionResult Index()
+        {
+            var players = this.Data.Players.All().OrderByDescending(p => p.Wins);
+            return View(players);
+        }
+    }
+}
