5d52f8f3cb24afe7b47ce13909957a559e8e83c2
RoYaLBG <royal4o@gmail.com>
2015-06-04 06:36:35 +0300

    Error pages defaults

--- CardReality/Global.asax.cs
+++ CardReality/Global.asax.cs
@@ -1,8 +1,12 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Diagnostics;
+using System.Net;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using CardReality.Controllers;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using Microsoft.WindowsAzure.Storage.Queue;
@@ -11,6 +15,12 @@ namespace CardReality
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public const string ErrorController = "Errors";
+        public const string NotFoundAction = "NotFound404";
+        public const string GeneralErrorAction = "Index";
+        public const string ErrorPageContentType = "text/html";
+        public const int NotFoundStatusCode = 404;
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -19,6 +29,39 @@ namespace CardReality
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             InitializeStorage();
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var lastError = Server.GetLastError();
+            Server.ClearError();
+            var routeData = new RouteData();
+            routeData.Values["controller"] = ErrorController;
+
+            
+            var isHttpException = lastError is HttpException;
+            var statusCode = -1;
+            if (isHttpException)
+            {
+                statusCode = (lastError as HttpException).GetHttpCode();
+            }
+
+            if (statusCode == NotFoundStatusCode)
+            {
+                Response.StatusCode = NotFoundStatusCode;
+                routeData.Values["action"] = NotFoundAction;
+            }
+            else
+            {
+                routeData.Values["action"] = GeneralErrorAction;
+            }
+              
+            Response.TrySkipIisCustomErrors = true; // IIS7 fix
+            Response.ContentType = ErrorPageContentType;
+            IController errorsController = new ErrorsController();
+            HttpContextWrapper wrapper = new HttpContextWrapper(Context);
+            var rc = new System.Web.Routing.RequestContext(wrapper, routeData);
+            errorsController.Execute(rc);
         }
 
         private void InitializeStorage()
